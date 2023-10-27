e5eea76bac1be363a3a62ab4c17bac625fb1bb66
mmlodi <mmischel@hotmail.com>
2023-10-26 22:42:58 -0300

    ?

--- src/main/java/br/net/razer/usuario/repository/ProdutoRepository.java
+++ src/main/java/br/net/razer/usuario/repository/ProdutoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.net.razer.usuario.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-    
+
 }
