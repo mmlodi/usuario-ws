82b5f1eb666c70c0ab800738ea0ca77751c6abe8
mmlodi <mmischel@hotmail.com>
2023-10-26 22:42:28 -0300

    adicionei arquivo para o cors da apṕlicação não sei se é a melhor maneira

---
+++ src/main/java/br/net/razer/usuario/CorsConfig.java
@@ -0,0 +1,20 @@
+package br.net.razer.usuario;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200") // Allow requests from localhost:4200
+            .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS"); // Specify the HTTP methods you want to allow
+    }
+
+
+}
